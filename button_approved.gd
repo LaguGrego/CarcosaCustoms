bc1ee9bff3ea06205df20cd3b8e8e7732d6409ec
LaguGrego <agusgregoret@gmail.com>
2026-06-04 17:35:12 -0300

    Nuevos sprites toolbar

---
+++ button_approved.gd
@@ -0,0 +1,18 @@
+extends Button
+
+@onready var luggage := $"../../Luggage"
+@onready var anim := $"../../Luggage/AnimationPlayer"
+
+func _ready() -> void:
+	pressed.connect(_button_pressed)
+
+func _button_pressed() -> void:
+	if anim.is_playing(): return
+	
+	anim.play("luggage_disappear")
+
+	await anim.animation_finished
+
+	luggage.pickTexture()
+
+	anim.play("luggage_appear")
---
+++ tool_6.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@export var cursor_texture: Texture2D
+
+func _ready():
+	input_pickable = true
+	self.input_event.connect(_on_input_event)
+
+func _on_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var imagen = cursor_texture.get_image()
+			# Calculás el nuevo tamaño al 15%
+			var nuevo_ancho = int(imagen.get_width() * 0.15)
+			var nuevo_alto = int(imagen.get_height() * 0.15)
+			imagen.resize(nuevo_ancho, nuevo_alto)
+			var nueva_textura = ImageTexture.create_from_image(imagen)
+			Input.set_custom_mouse_cursor(nueva_textura)
+
+
+
+	
