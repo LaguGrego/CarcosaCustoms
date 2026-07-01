bcd222271ea2a0b427dbc8f5db38e4f643f5437d
LaguGrego <agusgregoret@gmail.com>
2026-07-01 14:44:35 -0300

    alarma visual numa

--- alarm.gd
+++ alarm.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AlarmTimer.set_wait_time(500 + randi() % 6)
+	$AlarmTimer.set_wait_time(30 + randi() % 30)
 	$AlarmTimer.start()
 	$AlarmAudio.set_volume_linear(9)
 	$ColorRect.z_index = 50
@@ -18,6 +18,7 @@ func _process(delta: float) -> void:
 func _on_timer_timeout() -> void:
 	$AlarmAudio.play()
 	$AnimationPlayer.play("alarm")
+	$AnimatedSprite2D.play("alarm_blink")
 	pass # Replace with function body.
 
 func _deactivate_alarm() -> void:
